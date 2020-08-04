482d4eff8a900fc879bd9f51c9cd74a3c352f6e0
peterchu999 <peter.andrew987@gmail.com>
2020-08-04 10:40:07 +0700

    initial push peter

--- MC3/AppDelegate.swift
+++ MC3/AppDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // test comment here
-
+    // peter push
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
